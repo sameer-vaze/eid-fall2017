14be409b31410f50420b0dbfcf2a68be6de5d83d
Sameer Vaze <sameer.vaze@colorado.edu>
2017-09-15 21:31:45 -0600

    Homework 1 updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # eid-fall2017
 Repo for Fall 2017 Embedded Interface Design class
+
+Author
+* Sameer Vaze
--- example.py
+++ example.py
@@ -19,14 +19,14 @@ import sys
 import os
 import datetime
 
-__author__ = "Bruce Montgomery and Bhallaji Venkatesankulathusundaram"
-__copyright__ = "Copyright (C) 2017 by Bruce Montgomery and Bhallaji Venkatesankulathusundaram"
+__author__ = "Sameer Vaze"
+__copyright__ = "Copyright (C) 2017 by Sameer Vaze"
 #
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
-# software. Bruce Montgomery, Bhallaji Venkatesankulathusundaram, and the University of Colorado
-# are not liable for any misuse of this material.
+# software. Sameer Vaze the University of Colorado are not liable for any 
+# misuse of this material.
 #
 
 class Dog(object):
@@ -45,7 +45,7 @@ class Dog(object):
 def walker(name):
     """walker: an example of a python function"""
     dog1 = Dog(name)
-    dog1.add_trick("Roll Over")
+    dog1.add_trick("Eat homework")
     print("Walking a new dog:")
     print("  The kind of this thing is ",dog1.kind)
     print("  This dog is ",dog1.name)
